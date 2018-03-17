fd5dd75d417f82a4851c090e42a3d4c469d1191a
Andrius Navasaitis <anavasaitis@gmail.com>
2018-03-17 20:41:37 +0000

    added detailed display of a meeting

--- src/manager/DiaryController.java
+++ src/manager/DiaryController.java
@@ -2,27 +2,28 @@ package manager;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXDialog;
+import com.jfoenix.controls.JFXDialogLayout;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.geometry.Insets;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
-import javafx.scene.text.Text;
-import org.controlsfx.control.PopOver;
 
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.ZoneId;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.LinkedList;
+import java.util.*;
 
 
 public class DiaryController {
@@ -34,26 +35,29 @@ public class DiaryController {
     @FXML public GridPane meetingsGrid;
     @FXML public TextField timeField;
     @FXML public TextField intervalField;
-    @FXML public AnchorPane rootDiaryPane;
+    @FXML public StackPane rootDiaryPane;
+    @FXML public GridPane columnHeads;
 
 
-    private Calendar startTime = Calendar.getInstance();
-    private Calendar interval = Calendar.getInstance();
-    private Calendar currentTime = Calendar.getInstance();
+    private Calendar startTime = Calendar.getInstance(Locale.getDefault());
+    private Calendar interval = Calendar.getInstance(Locale.getDefault());
+    private Calendar currentTime = Calendar.getInstance(Locale.getDefault());
     private Database db = new Database();
-    private int employeeID;
+    private int employeeID = 1;
     private LinkedList<Event> events;
+    // TODO: 17/03/2018 possibly introduce a new class to store pane and event reference 
+    private LinkedList<Event> displayedEvents = new LinkedList<>();
+    private LinkedList<StackPane> panes = new LinkedList<>();
+    private Node currentDayIndic;
+    private int[] weekdayTranslate = {0, 7, 1, 2, 3, 4, 5, 6};
 
     @FXML
     void initialize() {
         db.setup();
         // TODO: 16/03/2018 change the way employeeID is stored and accessed on a top level
-        employeeID = Main.getCurrentEmployeeID();
-        events = db.getEmployeesEvents(employeeID);
+        events = db.getEmployeeEvents(employeeID);
 
-        for(Event e : events) {
-            e.print();
-        }
+
 
         AnchorPane.setRightAnchor(rootDiaryPane, 0.0);
         AnchorPane.setBottomAnchor(rootDiaryPane, 0.0);
@@ -61,70 +65,85 @@ public class DiaryController {
         AnchorPane.setTopAnchor(rootDiaryPane, 0.0);
 
         currentWeek.setText(String.format("WEEK %d", currentTime.get(Calendar.WEEK_OF_MONTH)));
+        currentDayIndic = columnHeads.getChildren().get(weekdayTranslate[currentTime.get(Calendar.DAY_OF_WEEK)]);
+        indicateCurrentDay();
         datePicker.setValue(LocalDate.now());
+
         startTime.set(Calendar.HOUR_OF_DAY, 8);
         startTime.set(Calendar.MINUTE, 0);
         interval.set(Calendar.MINUTE, 30);
 
+        System.out.println(Locale.getDefault() + ": " + startTime.getFirstDayOfWeek());
         updateTimeInterval();
         displayMeetings();
-//        meetingsGrid.setGridLinesVisible(true);
-//        Pane p = new Pane(new Label("Test meeting"));
-//
-//        p.setStyle("-fx-background-color: grey");
-//        p.addEventHandler(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
-//            @Override
-//            public void handle(MouseEvent event) {
-//                System.out.print("test");
-//                constructDialog(p);
-////                JFXDialog d = constructDialog();
-////                d.show();
-//            }
-//        });
-//        meetingsGrid.add(p, 6, 123, 1, 45);
 
     }
+    private void constructDialog(Event e) {
+        JFXDialogLayout layout = new JFXDialogLayout();
 
-    private void setupGrid() {
-        int hour = startTime.get(Calendar.HOUR_OF_DAY);
-        int minutes = startTime.get(Calendar.MINUTE);
+        StackPane rootPane = null;
+        try {
+            rootPane = FXMLLoader.load(getClass().getResource("/fxml/meetingDetails.fxml"));
+        } catch (Exception ex) {
+            System.err.println( ex.getClass().getName() + ": " + ex.getMessage() );
+        }
 
+        Label label = (Label) rootPane.lookup("#currentDate");
+        label.setText(new SimpleDateFormat("YYYY MM dd").format(currentTime.getTime()));
 
-    }
-    //// TODO: 13/03/2018 pass params to construct the dialog 
-    private void constructDialog(Pane p) {
-        PopOver pop = new PopOver();
-        VBox box = new VBox();
-        box.setPadding(new Insets(10));
+        label = (Label) rootPane.lookup("#currentTime");
+        label.setText(new SimpleDateFormat("HH:mm").format(currentTime.getTime()));
 
-        box.getChildren().add(new Text("test"));
+        label = (Label) rootPane.lookup("#meetingSubject");
+        label.setText(e.getTitle());
 
-        pop.setContentNode(box);
+        label = (Label) rootPane.lookup("#meetingStart");
+        label.setText(new SimpleDateFormat("HH:mm").format(e.getStart()));
 
-        pop.show(p);
-//        JFXDialogLayout content = new JFXDialogLayout();
-//        content.setHeading(new Text("heading"));
-//        content.setBody(new Text("some information about the meeting goes here"));
-//        StackPane stackPane = new StackPane();
-//        JFXDialog dialog = new JFXDialog(stackPane, content, JFXDialog.DialogTransition.CENTER);
-//        JFXButton button = new JFXButton("Okay");
-//        button.setOnAction(new EventHandler<ActionEvent>() {
-//            @Override
-//            public void handle(ActionEvent event) {
-//                dialog.close();
-//            }
-//        });
-//        content.setActions(button);
-//        Scene scene = new Scene(stackPane, 300, 250);
-////        meetingsGrid.
-//        dialog.show();
-//        return dialog;
+        label = (Label) rootPane.lookup("#meetingEnd");
+        label.setText(new SimpleDateFormat("HH:mm").format(e.getEnd()));
+
+        label = (Label) rootPane.lookup("#meetingDate");
+        label.setText(new SimpleDateFormat("YYYY-MM-dd").format(e.getStart()));
+
+        label = (Label) rootPane.lookup("#meetingDuration");
+        long duration = (e.getEnd().getTime() - e.getStart().getTime()) / 60000;
+        label.setText(String.format("%d minutes", duration));
+
+        label = (Label) rootPane.lookup("#meetingLocation");
+        label.setText(e.getLocation());
+
+        label = (Label) rootPane.lookup("#meetingDescription");
+        label.setText(e.getDesc());
+
+        label = (Label) rootPane.lookup("#meetingAttendees");
+        LinkedList<Employee> attendees = db.getEventAttendees(e.getId());
+
+        StringJoiner joiner = new StringJoiner(", ");
+        for(Employee employee : attendees) {
+            joiner.add(employee.getName());
+        }
+        label.setText(joiner.toString());
+
+        layout.setHeading(rootPane);
+
+        JFXDialog dialog = new JFXDialog(rootDiaryPane, layout, JFXDialog.DialogTransition.CENTER);
+        dialog.focusedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                if(newValue) {
+                    dialog.close();
+                }
+            }
+        });
+
+        dialog.show();
     }
     private void updateTimeInterval() {
         meetingsGrid.getChildren().clear();
         DateFormat df = new SimpleDateFormat("HH:mm");
         Calendar cal = Calendar.getInstance();
-        int h = startTime.get(Calendar.HOUR);
+        int h = startTime.get(Calendar.HOUR_OF_DAY);
         int m = startTime.get(Calendar.MINUTE);
         int inter = interval.get(Calendar.MINUTE);
         cal.set(Calendar.HOUR_OF_DAY, h);
@@ -217,16 +236,15 @@ public class DiaryController {
         switch(id) {
             case "incWeek":
                 currentTime.add(Calendar.WEEK_OF_MONTH, 1);
-                // TODO: 14/03/2018 increase week
                 break;
             case "decWeek":
                 currentTime.add(Calendar.WEEK_OF_MONTH, -1);
-                // TODO: 14/03/2018 decrease week
                 break;
         }
         currentWeek.setText(String.format("WEEK %d", currentTime.get(Calendar.WEEK_OF_MONTH)));
         Date input = currentTime.getTime();
         datePicker.setValue(input.toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+        displayMeetings();
     }
 
     public void dateChosen(ActionEvent actionEvent) {
@@ -234,9 +252,12 @@ public class DiaryController {
                 datePicker.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant()));
         System.out.println(currentTime.getTime().toInstant());
         currentWeek.setText(String.format("WEEK %d", currentTime.get(Calendar.WEEK_OF_MONTH)));
+        indicateCurrentDay();
+        displayMeetings();
     }
 
     private void displayMeetings() {
+        clearMeetings();
         for(Event e : events) {
             Calendar start = Calendar.getInstance();
             start.setTime(e.getStart());
@@ -252,33 +273,71 @@ public class DiaryController {
             int endM = end.get(Calendar.MONTH);
             int endD = end.get(Calendar.DAY_OF_MONTH);
 
-//            System.out.println(start.getTime().toInstant());
-//            System.out.println(end.getTime().toInstant());
-
             if(startY == endY && startM == endM && startD == endD) {
-//                System.out.println("YYYY-MM-DD matches");
-//                System.out.printf("\nstarting hour: %d\nstarting minute: %d", startTime.get(Calendar.HOUR_OF_DAY), startTime.get(Calendar.MINUTE));
-//                System.out.printf("\nevent starting hour: %d\nevent starting minute: %d", start.get(Calendar.HOUR_OF_DAY), start.get(Calendar.MINUTE));
-                if(start.get(Calendar.HOUR_OF_DAY) >= startTime.get(Calendar.HOUR_OF_DAY)) {
+                if(start.get(Calendar.HOUR_OF_DAY) >= startTime.get(Calendar.HOUR_OF_DAY) &&
+                        start.get(Calendar.WEEK_OF_YEAR) == currentTime.get(Calendar.WEEK_OF_YEAR)) {
+
                     System.out.println("Adding meeting");
+                    displayedEvents.add(e);
                     int[] meetingPos = getMeetingPosition(start, end);
-                    Pane pane = new Pane(new Label(e.getTitle()));
-                    pane.setStyle("-fx-background-color: grey");
+                    StackPane pane = createMeetingPane(e);
+                    pane.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+                        @Override
+                        public void handle(MouseEvent event) {
+                            StackPane p = (StackPane) event.getSource();
+                            constructDialog(displayedEvents.get(panes.indexOf(p)));
+                            System.out.printf("\nEvent: %s", displayedEvents.get(panes.indexOf(p)).getTitle());
+                        }
+                    });
+                    panes.add(pane);
                     meetingsGrid.add(pane, meetingPos[0], meetingPos[1], 1, meetingPos[2]);
                 }
             }
         }
     }
 
+    private StackPane createMeetingPane(Event event) {
+        // TODO: 17/03/2018 add formating 
+        StackPane pane = new StackPane();
+        
+        GridPane grid = new GridPane();
+
+        Label label = new Label(event.getTitle());
+        grid.add(label, 0, 0);
+        label = new Label(String.format("Start: %s", new SimpleDateFormat("HH:mm").format(event.getStart())));
+        grid.add(label, 0, 1);
+        label = new Label(String.format("End: %s", new SimpleDateFormat("HH:mm").format(event.getEnd())));
+        grid.add(label, 0, 2);
+        long duration = (event.getEnd().getTime() - event.getStart().getTime()) / 60000;
+        label = new Label(String.format("Duration: %d min", duration));
+        grid.add(label, 0, 3);
+        // TODO: 17/03/2018 add coloring based on priority
+        pane.setStyle("-fx-background-color: grey");
+        pane.getChildren().addAll(grid);
+
+        return pane;
+    }
+
     private int[] getMeetingPosition(Calendar start, Calendar end) {
-        int minutes = ((end.get(Calendar.HOUR_OF_DAY) * 60 + end.get(Calendar.MINUTE) -
-                start.get(Calendar.HOUR_OF_DAY) * 60 + start.get(Calendar.MINUTE)));
+        int minutes = (((end.get(Calendar.HOUR_OF_DAY) * 60 + end.get(Calendar.MINUTE)) -
+                (start.get(Calendar.HOUR_OF_DAY) * 60 + start.get(Calendar.MINUTE))));
 
         int startMinute = start.get(Calendar.HOUR_OF_DAY) * 60 + start.get(Calendar.MINUTE) -
                 startTime.get(Calendar.HOUR_OF_DAY) * 60 - startTime.get(Calendar.MINUTE);
 
         int dayColumn = start.get(Calendar.DAY_OF_WEEK) - 1;
-        System.out.printf("\nMinutes: %d\nstartMinute: %d\ndayColumn: %d", minutes, startMinute, dayColumn);
         return new int[]{dayColumn, startMinute, minutes};
+    }
+
+    private void clearMeetings() {
+        for(Pane p : panes) {
+            meetingsGrid.getChildren().remove(p);
+        }
+    }
+
+    private void indicateCurrentDay() {
+        currentDayIndic.setStyle("-fx-border-width: 0 0 1 0; -fx-border-color: black");
+        currentDayIndic = columnHeads.getChildren().get(weekdayTranslate[currentTime.get(Calendar.DAY_OF_WEEK)]);
+        currentDayIndic.setStyle("-fx-border-width: 0 0 3 0; -fx-border-color: green");
     }
 }
