301fe5da11edabc4c8027436332615b6ef36c8d3
Andrius Navasaitis <anavasaitis@gmail.com>
2018-03-17 20:43:25 +0000

    temporary disabled login window

--- src/manager/Main.java
+++ src/manager/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/startup.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/diary.fxml"));
         Parent root = loader.load();
         primaryStage.setTitle("Meetings Manager");
         primaryStage.setScene(new Scene(root, 1200, 1000));
